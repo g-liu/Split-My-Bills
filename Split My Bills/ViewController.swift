aa6091528dcea7e7a8dc0b128e295805b4da1ae8
Geoffrey Liu <geoff@paypal.com>
2021-10-12 11:18:54 -0700

    Initial Commit

---
+++ Split My Bills/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Split My Bills
+//
+//  Created by Geoffrey Liu on 10/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
